596b6fda7ba70b4bf2c8b321085b71805c9bc2c1
gameguy <vipulkumarpandya@gmail.com>
2018-09-06 21:22:48 -0500

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,4 @@
+ a = input("Enter sequence of comma separated numbers : ")
+ lst = []
+ lst = a
+ print(lst)
---
+++ problem4.py
@@ -0,0 +1,4 @@
+print("WE, THE PEOPLE OF INDIA,", "\n"  ,
+       "     having solemnly resolved to constitute India into a SOVEREIGN,!" , "\n",
+       "            SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC " ,"\n",
+       "             and to secure to all its citizens")
